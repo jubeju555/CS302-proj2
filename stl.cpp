94bee9cda848d8c1967e9efddbd6af84e3728934
Keaton William Wyrick <kwyrick4@vols.utk.edu>
2025-02-02 20:55:12 -0500

    hopefully fixed

--- stl.cpp
+++ stl.cpp
@@ -1,7 +1,6 @@
 // stl.cpp
 
 #include "volsort.h"
-
 #include <algorithm>
 #include <iostream>
 #include <vector>
@@ -11,39 +10,29 @@ using namespace std;
 bool node_string_compare(const Node* a, const Node* b){
     return a->string > b->string;
 }
-
+     
 //true if first is greater
 bool node_number_compare(const Node* a, const Node* b){
     return a->number > b->number;
 }
-
+     
 void stl_sort(List &l, bool numeric) {
     Node* head = l.head;
     vector<Node*> vec;
-    if(numeric){
-        while(head!=nullptr){
-            vec.push_back(head);
-            head = head->next;
-        }
-    }
-    else{
-        while(head!=nullptr){
-            vec.push_back(head);
-            head = head->next;
-        }
+    while(head->next!=nullptr){
+        vec.push_back(head);
+        head = head->next;
     }
     //if numeric, sort by int. if not sort by string
-    if(numeric) sort(vec.begin(), vec.end(), node_string_compare);
+    if(numeric) sort(vec.begin(), vec.end(), node_number_compare);
     else sort(vec.begin(), vec.end(), node_string_compare);
-
+     
     //make the list the same order as the vec
     l.head = vec[0];
-    for(size_t i=0; i<vec.size(); i++){
+    for(size_t i=0; i<vec.size()-1; i++){
         vec[i]->next = vec[i+1];
-        cout<<vec[i]->string;
+        // cout<<vec[i]->string;
     }
     vec[vec.size()-1]->next = nullptr;
     head = l.head;
 }
-
-
