73bc7283a1e992e50ec8d07be7bf1c3a2bf2aaf1
judah benjamin <142185545+jubeju555@users.noreply.github.com>
2025-02-05 18:22:10 +0000

    modified:   qsort.cpp

--- qsort.cpp
+++ qsort.cpp
@@ -6,14 +6,38 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+struct Node
+{
+    string string;
+    int number;
+    Node *next;
+};
+struct List
+{
+    Node *head;
+};
 void qsort_sort(List &l, bool numeric, void *base, size_t num, size_t width, int compare)
 {
     if (l.head == nullptr)
     {
         return;
     }    
-    qsort(base, num, width, compare);
-    Node *n1 = new Node;
+    if (num <= 1)
+    {
+        return;
+    }
+    vector<Node *> vec;
+    Node *current = l.head;
+    while (current != nullptr)
+    {
+        vec.push_back(current);
+        current = current->next;
+    }
+}
+
+
+int main(){
+ Node *n1 = new Node;
     n1->string = "a";
     n1->number = 1;
     Node *n2 = new Node;
@@ -34,28 +58,28 @@ void qsort_sort(List &l, bool numeric, void *base, size_t num, size_t width, int
 
     List l;
     l.head = n4;
+    return 0;
+}    
+// qsort_sort(l, true, l.head, 4, sizeof(Node), [numeric](const void *a, const void *b) -> int
+    //  {
+    //     Node *na = (Node *)a;
+    //     Node *nb = (Node *)b;
+    //     if (numeric) {
+    //         return na->number - nb->number;
+    //     } else {
+    //         return na->string.compare(nb->string);
+    //     }
+    // });
 
-    qsort_sort(l, true, l.head, 4, sizeof(Node), [numeric](const void *a, const void *b) -> int
-     {
-        Node *na = (Node *)a;
-        Node *nb = (Node *)b;
-        if (numeric) {
-            return na->number - nb->number;
-        } else {
-            return na->string.compare(nb->string);
-        }
-    });
+    // Node *current = l.head;
+    // while (current != nullptr) {
+    //     cout << current->string << "->";
+    //     current = current->next;
+    // }
+    // cout << current->string << "\n";
 
-    Node *current = l.head;
-    while (current != nullptr) {
-        cout << current->string << "->";
-        current = current->next;
-    }
-    cout << current->string << "\n";
-
-    while (l.head != nullptr) {
-        Node *temp = l.head;
-        l.head = l.head->next;
-        delete temp;
-    }
-}
+    // while (l.head != nullptr) {
+    //     Node *temp = l.head;
+    //     l.head = l.head->next;
+    //     delete temp;
+    // }
