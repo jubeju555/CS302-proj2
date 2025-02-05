03715da64b23ce48706b075068380ed57dc97def
judah benjamin <142185545+jubeju555@users.noreply.github.com>
2025-02-05 14:06:57 +0000

    new file:   qsort 	modified:   qsort.cpp

--- qsort.cpp
+++ qsort.cpp
@@ -7,60 +7,18 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-// void List::qsort_sort(List &l, bool numeric, void *base, size_t num, size_t width, int (*compare)(const void *, const void *))
-// {
-//     // qsort(base, num, width, compare);
-//     Node *head = (Node *)base;
-//     Node *tail = (Node *)base;  
-//     if (l.head == nullptr)
-//     {
-//         return;
-//     }    
-//     qsort_sort(l, numeric, base, num, width, compare);
+void List::qsort_sort(List &l, bool numeric, void *base, size_t num, size_t width, int (*compare)(const void *, const void *))
+{
+    // qsort(base, num, width, compare);
+    Node *head = (Node *)base;
+    Node *tail = (Node *)base;
+    if (l.head == nullptr)
+    {
+        return;
+    }    
+    qsort_sort(l, numeric, base, num, width, compare);
 
-// }
-// int main()
-// {
-//     Node *n1 = new Node;
-//     n1->string = "a";
-//     n1->number = 1;
-//     Node *n2 = new Node;
-//     n2->string = "b";
-//     n2->number = 2;
-//     Node *n3 = new Node;
-//     n3->string = "c";
-//     n3->number = 3;
-//     Node *n4 = new Node;
-//     n4->string = "d";
-//     n4->number = 4;
-//     // order: (4, 1, 3, 2)
-//     n2->next = n1;
-//     n1->next = n3;
-// //     qsort_sort(l, numeric, base, 
-//     Node *head = (Node *)base;
-//     Node *tail = (Node *)base;n
-        
-//         l.qsort_sort(l, true, l.head, 4, sizeof(Node), [](const void *a, const void *b) -> int    n3->next = n4
-//     qsort_sort(l, numeric, base, num, width, compare);;
-//     n4->next = nullptr;
-
-//     List l;
-// l.head = n4;
-//     l.qsort_sort(l, true, l.head, 4, sizeof(Node), [](const void *a, const void *b) -> int
-//                  {
-//         Node *na = (Node *)a;
-//         Node *nb = (Node *)b;
-//         return na->string.compare(nb->string); });
-//     Node *head = l.head;
-//     while (head->next != nullptr)
-//     {
-//         cout << head->string << "->";
-//         head = head->next;
-//     }
-//     std::cout << head->string << "\n";
-//     return 0;
-// }
-
+}
 int main()
 {
     // Create test nodes
