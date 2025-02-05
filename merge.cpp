7d6369dc8250f8227ff4fd038d4104c86d500c32
Keaton William Wyrick <kwyrick4@vols.utk.edu>
2025-02-05 10:23:33 -0500

    committing for merge to get volsort back

--- merge.cpp
+++ merge.cpp
@@ -12,15 +12,15 @@ Node *merge(Node *left, Node *right, bool numeric);
 
 // Implementations
 
-void merge_sort(List &l, bool numeric) {
-}
-
-Node *msort(Node *head, bool numeric) {
-}
-
-void split(Node *head, Node *&left, Node *&right) {
-}
-
-Node *merge(Node *left, Node *right, bool numeric) {
-}
+// void merge_sort(List &l, bool numeric) {
+// }
+//
+// Node *msort(Node *head, bool numeric) {
+// }
+//
+// void split(Node *head, Node *&left, Node *&right) {
+// }
+//
+// Node *merge(Node *left, Node *right, bool numeric) {
+// }
 
