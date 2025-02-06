b1ff3bf5e42e6381e332bcfee00b5296b79dfd19
judah benjamin <142185545+jubeju555@users.noreply.github.com>
2025-02-06 14:18:05 +0000

    new file:   .vscode/settings.json 	modified:   quick.cpp 	modified:   quicksort

--- quick.cpp
+++ quick.cpp
@@ -3,7 +3,7 @@
 #include "volsort.h"
 
 #include <iostream>
-
+using namespace std;
 // Prototypes
 
 Node *qsort(Node *head, bool numeric);
@@ -107,14 +107,19 @@ int main(){
     n3->next = n4;
     n4->next = nullptr;
 
+
+
+
     List l;
     l.head = n4;
     quick_sort(l, false);
     Node* head = l.head;
     while(head->next != nullptr){
-        std::cout<<head->string<<"->";
+    cout<<head->string<<"->";
         head = head->next;
     }
-    std::cout<<head->string<<"\n";
+    quick_sort(l, true);
+    head = l.head;
+    cout << head->string << "\n";
     return 0;
 }
