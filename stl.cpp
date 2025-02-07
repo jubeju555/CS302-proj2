f244a92568efec07faa67b0dd86c7727f9dd0f3f
judah benjamin <142185545+jubeju555@users.noreply.github.com>
2025-02-07 15:26:37 +0000

    ok make file works on main branch, stl needs to be pushed in as well as everything else

--- stl.cpp
+++ stl.cpp
@@ -43,27 +43,27 @@ void stl_sort(List &l, bool numeric) {
     vec[vec.size()-1]->next = nullptr;
     head = l.head;
 }
-int main(){
-    //initializing
-    Node* n1 = new Node;
-    n1->string = "a";
-    n1->number = 1;
-    Node* n2 = new Node;
-    n2->string = "b";
-    n2->number = 2;
-    Node* n3 = new Node;
-    n3->string = "c";
-    n3->number = 3;
-    Node* n4 = new Node;
-    n4->string = "d";
-    n4->number = 4;
-    //ordering (4, 1, 3, 2)
-    n2->next = n1;
-    n1->next = n3;
-    n3->next = n4;
-    n4->next = nullptr;
+// int main(){
+//     //initializing
+//     Node* n1 = new Node;
+//     n1->string = "a";
+//     n1->number = 1;
+//     Node* n2 = new Node;
+//     n2->string = "b";
+//     n2->number = 2;
+//     Node* n3 = new Node;
+//     n3->string = "c";
+//     n3->number = 3;
+//     Node* n4 = new Node;
+//     n4->string = "d";
+//     n4->number = 4;
+//     //ordering (4, 1, 3, 2)
+//     n2->next = n1;
+//     n1->next = n3;
+//     n3->next = n4;
+//     n4->next = nullptr;
 
-    List l;
-    l.head = n4;
-    stl_sort(l, false);
-}
+//     List l;
+//     l.head = n4;
+//     stl_sort(l, false);
+// }
