22fc54d96456cf6e254bcb6d62c839e577cc6ec3
Keaton William Wyrick <kwyrick4@vols.utk.edu>
2025-02-10 22:44:16 -0500

    pushing 100% test cases

---
+++ makeinput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int num = 0;
+    int count = 0;
+    ofstream fout = (input_25000000.txt);
+    while(count<=25000000){
+        fout<<math.rand()*25000000
+    }
+}
